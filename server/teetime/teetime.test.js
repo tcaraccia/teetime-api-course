c125555b765f846ac731f8bb7bb70ff5bb7b8fc7
tcaraccia <tomascaraccia@gmail.com>
2018-06-18 01:48:12 -0300

    fix test

--- server/teetime/teetime.test.js
+++ server/teetime/teetime.test.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
-const request = require('supertest-as-promised');
-const httpStatus = require('http-status');
 const chai = require('chai'); // eslint-disable-line import/newline-after-import
 const chaidate = require('chai-datetime');
 
 chai.use(chaidate);
-
-const expect = chai.expect;
-const app = require('../../index');
 
 chai.config.includeStack = true;
 
@@ -22,13 +17,4 @@ after((done) => {
 });
 
 describe('## Teetime APIs', () => {
-  let teetime = {
-    date: Date.now(),
-    times: [
-      {
-        date: Date.now,
-        players: ['3589878']
-      }
-    ]
-  };
 });
